f9ab55bfecdc2fb9c9a180b873cbb3a49e29acfe
Jhon Rojo <jhonharoldrojo@gmail.com>
2024-11-21 12:42:33 -0500

    menu sup git

--- app.py
+++ app.py
@@ -28,17 +28,46 @@ def transportes():
 @app.route('/bar-disco')
 def bardisco():
      return render_template("bardisco.html")
+ 
+@app.route('/gastronomia')
+def gastronomia():
+     return  "gastronomia"
+ 
+@app.route('/tours')
+def tours():
+    return ("tours")
 
+@app.route('/playas')
+def playas():
+    return ("playas")
 
 @app.route('/contacto')
 def contacto():
-     return render_template("contacto.html")
+    return render_template("contacto.html")
 
+@app.route('/atracciones')
+def atracciones():
+    return ("atracciones")
+
+@app.route('/promociones')
+def promociones():
+    return ("promociones")
+
+@app.route('/mapa')
+def mapa():
+    return ("mapa")
 
 
 @app.route('/saludo/<nombre>')
 def saludo_nombre(nombre):
-    return f"hola, {nombre}"
+    return "hola, {nombre}"
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
+    
+    
+    
+     
+                 
+                 
+                
