8e2b97147ff5c1bd9f651267424019cca990cf41
Jhon Rojo <jhonharoldrojo@gmail.com>
2024-11-22 12:29:56 -0500

    meus

--- app.py
+++ app.py
@@ -16,26 +16,19 @@ def restaurantes():
    return render_template("restaurantes.html")
 
 
-@app.route('/eventos')
-def eventos():
-    return render_template("eventos.html")
 
 @app.route('/transportes')
 def transportes():
     return render_template("transportes.html")
 
 
-@app.route('/bar-disco')
-def bardisco():
-     return render_template("bardisco.html")
- 
-@app.route('/gastronomia')
-def gastronomia():
-     return  "gastronomia"
+@app.route('/sitios-turisticos')
+def sitiosturisticos():
+    return render_template("sitiosturisticos.html")
  
 @app.route('/tours')
 def tours():
-    return ("tours")
+   return render_template("tours.html")
 
 @app.route('/playas')
 def playas():
@@ -45,18 +38,9 @@ def playas():
 def contacto():
     return render_template("contacto.html")
 
-@app.route('/atracciones')
-def atracciones():
-    return ("atracciones")
-
-@app.route('/promociones')
-def promociones():
-    return ("promociones")
-
 @app.route('/mapa')
 def mapa():
-    return ("mapa")
-
+   return render_template ("mapa.html")
 
 @app.route('/saludo/<nombre>')
 def saludo_nombre(nombre):
