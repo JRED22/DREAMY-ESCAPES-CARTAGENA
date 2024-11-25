7d5f574939773083f8ad598d899822cd517f0fb5
AndreaGomezS1 <andrea.gsy2305@gmail,com>
2024-11-24 20:49:26 -0500

    botones de restaurantes y tarjetas

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// Función para filtrar restaurantes según el rango de precios
+function filterRestaurants(priceRange) {
+    const cards = document.querySelectorAll('.card'); // Selecciona todas las tarjetas
+    cards.forEach((card) => {
+      // Compara el atributo data-price de cada tarjeta con el rango de precios seleccionado
+      if (card.getAttribute('data-price') === priceRange || priceRange === 'all') {
+        card.style.display = 'block'; // Muestra la tarjeta si coincide
+      } else {
+        card.style.display = 'none'; // Oculta la tarjeta si no coincide
+      }
+    });
+  }
+  
