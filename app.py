6145b0aebade214daf536f1e896f56b8d8044cde
Jhon Rojo <jhonharoldrojo@gmail.com>
2024-11-29 10:21:14 -0500

    modificacion ajax

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 from flask import Flask, render_template,request
+import json, os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("base.html", titulo="Página de Inicio")
-
-@app.route('/a')
-def index2():
-    return render_template("index2.html")
-
+    return render_template("index.html", titulo="Página de Inicio")
 
 @app.route('/destinos')
 def destino():
+   
     return render_template("destinos.html")
 
 
 @app.route('/restaurantes')
 def restaurantes():
-   return render_template("restaurantes.html")
+    return render_template("restaurantes.html")
 
 
 
 @app.route('/transportes')
 def transportes():
-    return render_template("transportes.html")
+     return render_template("transportes.html")
 
 
 @app.route('/sitios-turisticos')
@@ -33,7 +30,7 @@ def sitiosturisticos():
  
 @app.route('/tours')
 def tours():
-   return render_template("tours.html",titulo="Tours")
+    return render_template("tours.html",titulo="Tours")
 
 @app.route('/playas')
 def playas():
@@ -43,7 +40,7 @@ def playas():
 
 @app.route('/contacto', methods=['GET'])
 def contacto():
-  return render_template("contacto.html", titulo="Contáctenos!!!")
+     return render_template("contacto.html", titulo="Contáctenos!!!")
 
 
 @app.route('/contacto', methods=['POST'])
@@ -53,24 +50,30 @@ def guardar_contacto():
   phone = request.form.get("inputPhone")
   message = request.form.get("inputMessage")
 
+
+  archivo_csv = "data/contactos.csv"
+  
   # Validaciones
   if name == "" or email == ""  or phone == ""  :
     validacion = "Error: No hay datos para contacto."
   else:
     # La información se almacenará en la base de datos
-    validacion = "Gracias por contactarnos, pronto nos pondremos en contacto con usted."
-
-  return render_template("base.html",
+    validacion = "Gracias por contactarnos, pronto nos pondremos en contacto con usted." 
+    # Crear el archivo si no existe, adicionando la cabecera
+    if os.path.exists(archivo_csv) == False:
+      with open(archivo_csv, "w") as archivo:
+        archivo.write("name,email,address1,address2,phone,city,state,zip,message\n")  
+    # Adicionar la información al archivo desde el formulario de contacto.
+    with open(archivo_csv, "a") as archivo:
+      archivo.write(f"{name},{email},{phone},{zip},{message}\n") 
+  return render_template("contacto.html",
                          titulo="Pronto te contactaremos.",
                          validacion=validacion)
 
 
-
-
-
 @app.route('/mapa')
 def mapa():
-   return render_template ("mapa.html")
+    return render_template ("mapa.html")
 
 @app.route('/team')
 def teams():
